f23960e2c248f9db003ad6d27105593935af4bb4
mjalara18 <mjalara18@gmail.com>
2023-03-15 21:50:48 -0500

    Create persona_alimento.routes.js

---
+++ app/routes/persona_alimento.routes.js
@@ -0,0 +1,17 @@
+module.exports = app => {
+    const personsFoods = require("../controllers/persona_alimento.controller.js");
+    var router = require("express").Router();
+  
+    router.post("/", personsFoods.create);
+  
+    router.get("/", personsFoods.findAll);
+
+    router.get("/:id", personsFoods.findOne);
+
+    router.delete("/:id", personsFoods.delete);
+
+    router.delete("/", personsFoods.deleteAll);
+
+    app.use('/api/personsFoods', router);
+
+  };

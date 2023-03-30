8c3470bbdee66340fbee885d1e337170671ebe30
Simón Way Esalas Young <simon.esalas@zabud.com.co>
2023-03-30 00:00:01 -0500

    Update person.routes.js

--- app/routes/person.routes.js
+++ app/routes/person.routes.js
@@ -4,7 +4,8 @@ module.exports = app => {
   
     router.post("/", persons.create);
   
-    router.get("/", persons.findAll);
+    router.get("/", persons.findAll); // http://localhost:1234/api/persons si lo dejas así con un get, el te va a buscar esta ruta, porque lo estás especificando
+    router.get("/findAll", persons.findAll); // http://localhost:1234/api/persons/finAdll si te das cuenta agregué una ruta que va a buscar tambien el findAll y es un get, pero con un nombre diferente. 
 
     router.get("/:id", persons.findOne);
 

155c8b6e35d5ca35a5b1a4c2c8d5099e016f8be1
Simón Way Esalas Young <simon.esalas@zabud.com.co>
2023-04-04 18:51:38 -0500

    Update banco.controllers.js

--- app/controllers/banco.controllers.js
+++ app/controllers/banco.controllers.js
@@ -3,6 +3,9 @@ const db = require("../models");
 const Banco = db.banco;
 
 exports.create = (req, res) => {
+
+    //req como argumento de solicitud, puede tener cuerpo o parametros a los cuales podemos accceder por medio de "body" y "params"
+
     if (!req.body.name1) {
       res.status(400).send({ message: "Content can not be empty!" });
       return;
@@ -29,36 +32,58 @@ exports.create = (req, res) => {
       gerente: req.body.gerente
     });
 
-   
-    banco
-      .save(banco)
-      .then(data => {
-        res.send(data);
+    //banco que es la importación del modelo, el cual te va a dar las propiedades para poder hacer las operaciones de base de datos
+    //tenemos el save, que viene siendo el guardado en la base de datos y luego (then) es decir, luego de que guarde, ejecutame cierta logica
+    // la información que obtenemos en data, es el resultado de la operación guardar y que por lo general retorna. 
+    banco.save(banco).then(data => {
+        res.send(data); // aquí usamos el argumento de respuesta res, para enviar una respuesta que en este caso va a ser la variable data que es la que me da la
+        //información guardara anteriormente. 
       })
-      .catch(err => {
+      .catch(err => { //catch funciona para capturar los errores y su variable de captura dentro de la funcion es err (aquí puedes poner cualquier cosa)
         res.status(500).send({
           message:
             err.message || "Some error occurred while creating the Banco."
-        });
+        });//lo mismo aquí, se retorna una respuesta por medio del res para que se muestre al momento que ocurra un error. 
       });
 };
 
+//tenemos el findAll que basicamente es buscar todos los registros guardados de esa entidad. 
+//recibiendo sus parametros de solicitud y respuesta igualmente. 
 exports.findAll = (req, res) => {
     const name1 = req.query.name1;
     var condition = name1 ? { name1: { $regex: new RegExp(name1), $options: "i" } } : {};
   
-    Banco.find(condition)
-    .then(data => {
+    //tenemos el metodo find en este caso que va a buscar todo. leugo de eso, la información que retorne se almacena en data y luego puedes manipularla antes de 
+    //retornarla por la variable res
+    Banco.find(condition).then(data => {
+
+        // dentro de los cuerpos puedes hacer todo tipo de operaciones, for, if, arreglos, asignaciones. 
+        // aquí es donde se modifica cuando se requiere la información incluso, se pueden crear funciones para realizar la operacion 
+
+        // Este era el requerimiento anterior. 
+        for(var i = 0; i< data.length; i++){
+          var row = data[i];
+          row.esRecorrido = true;
+        }
+
         res.send(data);
-      })
-      
-    .catch(err => {
+        //processEjemplo(data);
+
+      }).catch(err => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while retrieving Banco."
       });
     });
 };
+
+//function processEjemplo(data){
+  //procesamos
+  //res.send(data);
+//}
+
+//en los demas metodos es lo mismo, se hace la consulta, se retorna la información y la puedes manipular, todo va a depender como esté el orden del 
+//proyecto, porque puede que la logica se pueda manejar desde otro archivo, pero siempre será llamada dentro de la ejecución. 
 
 exports.findOne = (req, res) => {
   const id = req.params.id;
